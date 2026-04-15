0a40063ef480ce03ba553c649dd1745db086045d
MavuriAlekhya2005 <22b61a0553@sitam.co.in>
2026-04-15 16:55:10 +0530

    Add DOM folder with dom.html and dom.js

---
+++ javascript/DOM/dom.js
@@ -0,0 +1,7 @@
+// dom.js
+const paragraphs = document.getElementsByTagName("p");
+
+// Update the text content of each <p>
+paragraphs[0].innerText = "Java";
+paragraphs[1].innerText = "HTML";
+paragraphs[2].innerText = "CSS";
